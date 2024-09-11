86a57e7e2a0fa9d04d02ae04fac9dc971fa39faf
kitakkun <48154936+kitakkun@users.noreply.github.com>
2024-09-11 16:14:02 +0900

    Implement habit-tracker App UI

--- habit-tracker/composeApp/src/commonMain/kotlin/com/jetbrains/workshop/App.kt
+++ habit-tracker/composeApp/src/commonMain/kotlin/com/jetbrains/workshop/App.kt
@@ -1,29 +1,19 @@
 package com.jetbrains.workshop
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
-import androidx.compose.runtime.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
-import kotlinproject.composeapp.generated.resources.*
-import kotlinproject.composeapp.generated.resources.Res
-import kotlinproject.composeapp.generated.resources.app_name
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import org.jetbrains.compose.resources.stringResource
 
 @Composable
 @Preview
@@ -42,25 +32,80 @@ fun App() {
         "Burpees",
     )
     MaterialTheme {
-        Box(Modifier.fillMaxSize().background(Color.White)) {
-            
+        Box(
+            Modifier.fillMaxSize().background(Color.White),
+            contentAlignment = Alignment.Center,
+        ) {
+            Column(
+                verticalArrangement = Arrangement.spacedBy(10.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text(
+                    text = "Habit Tracker",
+                    style = MaterialTheme.typography.h1,
+                )
+                exercises.forEach {
+                    MyElement(
+                        label = it,
+                        count = 10,
+                        onUpdateLabel = {},
+                        onClickMinus = {},
+                        onClickPlus = {},
+                    )
+                }
+            }
         }
     }
 }
 
 @Composable
-fun MyElement(label: String) {
-    Column {
-        Text(label)
-        Text(label)
-        Text(label)
+fun MyElement(
+    label: String,
+    count: Int,
+    onUpdateLabel: (String) -> Unit,
+    onClickMinus: () -> Unit,
+    onClickPlus: () -> Unit,
+) {
+    Row(
+        modifier = Modifier
+            .border(
+                width = 1.dp,
+                color = Color.Black,
+                shape = RoundedCornerShape(10.dp)
+            )
+            .padding(10.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(
+            10.dp,
+        )
+    ) {
+        TextField(label, onUpdateLabel)
+        IconButton(onClick = onClickMinus) {
+            Icon(
+                Icons.Default.Delete,
+                null,
+            )
+        }
+        Text(count.toString())
+        IconButton(onClick = onClickPlus) {
+            Icon(
+                Icons.Default.Add,
+                null,
+            )
+        }
     }
 }
 
 @Preview
 @Composable
 fun MyElementPreview() {
-    MyElement("Hello")
+    MyElement(
+        "Hello",
+        count = 10,
+        onUpdateLabel = {},
+        onClickMinus = {},
+        onClickPlus = {},
+    )
 }
 
 
