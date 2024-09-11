fa081be01505934a0053499a7ef4c5e2092ba61c
kitakkun <48154936+kitakkun@users.noreply.github.com>
2024-09-11 15:20:39 +0900

    Implement birds app with Ktor

--- bird-app/shared/src/commonMain/kotlin/com/jetbrains/workshop/BirdRepository.kt
+++ bird-app/shared/src/commonMain/kotlin/com/jetbrains/workshop/BirdRepository.kt
@@ -1,17 +1,23 @@
 import com.jetbrains.workshop.BirdImage
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
+import io.ktor.client.engine.*
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.get
 import io.ktor.serialization.kotlinx.json.json
 
 class BirdRepository {
-    // TODO: Get a list of images from this endpoint: "https://sebi.io/demo-image-api/pictures.json"
+    private val client = HttpClient {
+        install(ContentNegotiation) {
+            json()
+        }
+    }
+
     suspend fun getImages(): List<BirdImage> {
-        return emptyList()
+        return client.get("https://sebi.io/demo-image-api/pictures.json").body<List<BirdImage>>()
     }
 
     fun close() {
-        
+        client.close()
     }
 }
